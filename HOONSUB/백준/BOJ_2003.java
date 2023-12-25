d4f6195debd9f73e8f00bb0e5e4f0006268fae61
Hoonsub Kim <kimhunsope@naver.com>
2023-12-25 13:25:40 +0900

    [1주차][HOONSUB] ✅

---
+++ HOONSUB/백준/BOJ_2003.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputData = reader.readLine().split(" ");
+        int N = Integer.parseInt(inputData[0]);
+        int M = Integer.parseInt(inputData[1]);
+
+        int[] numbers = new int[N];
+        String[] inputNumbers = reader.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(inputNumbers[i]);
+        }
+
+        int answer = 0;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+
+        while (start < N) {
+            while (end < N && sum < M) {
+                sum += numbers[end];
+                end++;
+            }
+            if (end >= N && sum < M) {
+                break;
+            }
+            if (sum == M) {
+                answer++;
+            }
+            sum -= numbers[start++];
+        }
+
+        System.out.println(answer);
+    }
+}
