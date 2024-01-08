4754d45c0872ff1ef5463e84cffc28786740efac
Hoonsub Kim <kimhunsope@naver.com>
2024-01-08 21:51:37 +0900

    [4주차][레벨2] - 로직 변경

--- HOONSUB/프로그래머스/PGS_258711.java
+++ HOONSUB/프로그래머스/PGS_258711.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] edges) {
         int[] answer = new int[4];
-        
+
         // step 1. 그래프 간선 정보 설정 및 In/Out Count 정보 설정
         Node[] map = new Node[1000001];
-        
+
         for (int[] edge : edges) {
             int from = edge[0];
             int to = edge[1];
@@ -16,7 +16,7 @@ class Solution {
             map[from].outCount++;
             map[to].inCount++;
         }
-        
+
         // step 2. 생성된 정점 찾기
         Node createdNode = null;
         for (Node node : map) {
@@ -26,39 +26,32 @@ class Solution {
                 break;
             }
         }
-        
-        // step 3. 생성된 정점에서 부터 그래프 탐색 시작
+
+        // step 3. 생성된 정점과 연결된 노드들을 제거
         int totalGraphCount = createdNode.outCount;
-        
+
         for (Node node : createdNode.outNodes) {
-            search(node, answer);
+            node.inCount--;
         }
+        map[createdNode.number] = null;
         
-        // step 4. 도넛 그래프 = 전체 그래프 수 - (막대 그래프 + 8자 그래프)
-        answer[1] = totalGraphCount - (answer[2] + answer[3]);
-        
-        return answer;
-    }
-    
-    private void search(Node node, int[] answer) {
-        node.isVisited = true;
-        
-        // 해당 노드가 8자 그래프인 경우
-        if (node.inCount >= 2 && node.outCount == 2) {
-            answer[3]++;
-            return;
-        }
-        
-        Node nextNode = node.getNextNode();
-        if (nextNode == null) {
-            // 막대 그래프인 경우 = 마지막 노드의 outCount가 0일때
-            if (node.outCount == 0) {
-                answer[2]++;
+        // step 4. 각 그래프에 맞는 노드를 확인
+        for (Node node : map) {
+            if (node != null) {
+                if (node.outCount == 0) {
+                    answer[2]++;
+                }
+                
+                if (node.outCount == 2 && node.inCount == 2) {
+                    answer[3]++;
+                }
             }
-            return;
         }
-        
-        search(nextNode, answer);
+
+        // step 5. 도넛 그래프 = 전체 그래프 수 - (막대 그래프 + 8자 그래프)
+        answer[1] = totalGraphCount - (answer[2] + answer[3]);
+
+        return answer;
     }
 }
 
@@ -66,20 +59,10 @@ class Node {
     int number;
     int outCount;
     int inCount;
-    boolean isVisited;
     List<Node> outNodes;
-    
+
     public Node(int number) {
         this.number = number;
         this.outNodes = new ArrayList<>();
-    }
-    
-    public Node getNextNode() {
-        for (Node node : outNodes) {
-            if (!node.isVisited) {
-                return node;
-            }
-        }
-        return null;
     }
 }
