3e2c2fecbece3c6a91d815c68527b1bdedc8a93d
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2024-01-23 21:32:18 +0900

    [6주차][실버3]

---
+++ TAEYUN/BAEKJOON/BOJ_15654.py
@@ -0,0 +1,23 @@
+#15654 S3
+
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().strip().split())
+
+nums = sorted(list(map(int, input().split())))
+
+def backtracking(tmp):
+    if len(tmp) == M:
+        print(*tmp)
+        return
+    
+    for i in range(0, N):
+        if nums[i] not in tmp:
+            tmp.append(nums[i])
+            backtracking(i, tmp)
+            tmp.pop()
+
+
+backtracking([])
